92a5db1aa6144042487ef969b99a3464235135bb
Geet Sethi <geetsethi05@gmail.com>
2021-01-20 11:28:01 +0530

    :construction: Added resolver for verifying a test case

--- packages/server/src/modules/testCase/testCaseResolver/index.ts
+++ packages/server/src/modules/testCase/testCaseResolver/index.ts
@@ -2,8 +2,10 @@ import { BuildSchemaOptions } from "type-graphql";
 
 import { CreateTestCaseResolver } from "./create";
 import { GetTestCaseResolver } from "./get";
+import { VerifyTestCaseResolver } from "./verify";
 
 export const testCaseResolvers: BuildSchemaOptions["resolvers"] = [
   CreateTestCaseResolver,
   GetTestCaseResolver,
+  VerifyTestCaseResolver,
 ];
---
+++ packages/server/src/modules/testCase/testCaseResolver/verify/verifyTestCaseResolver.ts
@@ -0,0 +1,26 @@
+import { Arg, Query, Resolver } from "type-graphql";
+
+import { UserRoles } from "@portal/common";
+
+import { isAuthenticated } from "../../../shared/decorators/isAuthenticated";
+
+import { TestCase } from "../../testCaseEntity";
+
+@Resolver(() => TestCase)
+export class VerifyTestCaseResolver {
+  @isAuthenticated([UserRoles.SUPERVISOR, UserRoles.ADMIN])
+  @Query(() => Boolean)
+  async verifyTestCase(@Arg("id") id: string): Promise<boolean> {
+    if (!id) {
+      return false;
+    }
+
+    try {
+      await TestCase.update({ id }, { verified: true });
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+}
