417e2ce12457ed29578287f406c280050760830e
Geet Sethi <geetsethi05@gmail.com>
2021-01-22 19:35:25 +0530

    :construction: Added resolver for getting a product

---
+++ packages/server/src/modules/masters/product/resolver/get/getProductResolver.ts
@@ -0,0 +1,23 @@
+import { Arg, Query, Resolver } from "type-graphql";
+
+import { isAuthenticated } from "../../../../shared/decorators";
+
+import { ProductMaster } from "../../productMasterEntity";
+
+@Resolver(() => ProductMaster)
+export class GetProductResolver {
+  @isAuthenticated()
+  @Query(() => ProductMaster, { nullable: true })
+  getProduct(@Arg("id") id: string): Promise<ProductMaster | undefined> {
+    if (!id) {
+      throw new Error("Id is required!");
+    }
+    return ProductMaster.findOne(id, { relations: ["createdBy", "updatedBy"] });
+  }
+
+  @isAuthenticated()
+  @Query(() => [ProductMaster])
+  getProducts(): Promise<ProductMaster[]> {
+    return ProductMaster.find({ relations: ["createdBy", "updatedBy"] });
+  }
+}
--- packages/server/src/modules/masters/product/resolver/index.ts
+++ packages/server/src/modules/masters/product/resolver/index.ts
@@ -1,7 +1,9 @@
 import { BuildSchemaOptions } from "type-graphql";
 
 import { CreateProductResolver } from "./create";
+import { GetProductResolver } from "./get";
 
 export const productResolvers: BuildSchemaOptions["resolvers"] = [
   CreateProductResolver,
+  GetProductResolver,
 ];
