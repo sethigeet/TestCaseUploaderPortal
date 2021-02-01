42c5563b5ba8829cb5aefaaa71f8a9af7bc14b29
Geet Sethi <geetsethi05@gmail.com>
2021-02-01 17:46:05 +0530

    :lipstick: Added the breadcrumbs in the wrapper for easier navigation

--- packages/web/src/modules/components/Wrapper.tsx
+++ packages/web/src/modules/components/Wrapper.tsx
@@ -1,6 +1,15 @@
 import { FC } from "react";
 
-import { Box, BoxProps } from "@chakra-ui/react";
+import { Link, useLocation } from "react-router-dom";
+
+import {
+  Box,
+  BoxProps,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+} from "@chakra-ui/react";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 import { ErrorMessageType } from "../types";
 
@@ -35,9 +44,64 @@ export const Wrapper: FC<WrapperProps> = ({
     });
   };
 
+  const location = useLocation();
+  const crumbs = location.pathname
+    .split("/")
+    .slice(1)
+    .map((c) => c.charAt(0).toUpperCase() + c.slice(1));
+
   return (
     <Box display="flex" flexDirection="column" minH="100vh">
       <Navbar />
+      {crumbs.length > 1 && (
+        <Box
+          mt={3}
+          mx={10}
+          p={3}
+          borderColor="gray.300"
+          borderTopWidth={1}
+          borderBottomWidth={1}
+        >
+          <Breadcrumb
+            spacing="8px"
+            separator={<ChevronRightIcon color="gray.500" />}
+          >
+            {crumbs.map((crumb, i, arr) => {
+              const isCurrentPage = i === arr.length - 1;
+
+              const toLink =
+                "/" +
+                arr
+                  .slice(0, i + 1)
+                  .map((c) => c.charAt(0).toLowerCase() + c.slice(1))
+                  .join("/");
+
+              return (
+                <BreadcrumbItem key={i}>
+                  {isCurrentPage ? (
+                    <BreadcrumbLink
+                      isCurrentPage
+                      fontWeight="bold"
+                      transform="scale(1.05)"
+                      pointerEvents="none"
+                    >
+                      {crumb}
+                    </BreadcrumbLink>
+                  ) : (
+                    <BreadcrumbLink
+                      as={Link}
+                      to={toLink}
+                      isCurrentPage={isCurrentPage}
+                    >
+                      {crumb}
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+              );
+            })}
+          </Breadcrumb>
+        </Box>
+      )}
       {loading && <LoadingIndicator loading={loading} />}
       {errorMessage && !errorIsToast && (
         <Box mt={4} minW={600} maxW={600} alignSelf="center">
